b44c9409a662d371ef8caf0f2bf5976216ce1514
adelwin <adelwin@gmail.com>
2015-05-23 14:17:45 +0800

    BLANK DRAFT

---
+++ photo-sender-automator/src/main/java/com/si/diamond/tools/photoSender/App.java
@@ -0,0 +1,13 @@
+package com.si.diamond.tools.photoSender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
